de227eafbe9efadf4f8ea41f89fcfa4b3a4b9d2f
SamuelPapyan <samuel.papyan170@gmail.com>
2023-08-05 16:10:02 +0400

    fixes in ex01

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -12,75 +12,78 @@
 
 #include "RPN.hpp"
 
-#include <cctype>
+#include <cctype> // std::isdigt(), std::isspace()
 
 std::stack<RPN::token_type> RPN::stack_;
 
 const struct RPN::operations RPN::operations_[op_size] = {
-    {'+', &RPN::opSum},
-    {'-', &RPN::opSub},
-    {'*', &RPN::opMul},
-    {'/', &RPN::opDiv}
+	{ '+', &RPN::opSum },
+	{ '-', &RPN::opSub },
+	{ '*', &RPN::opMul },
+	{ '/', &RPN::opDiv }
 };
 
 RPN::result_type RPN::resolveExpression(const std::string &line)
 {
-    for (std::string::const_iterator it = line.begin(); it != line.end(); ++it) {
-        if (std::isspace(*it))
-            continue;
-        else if (std::isdigit(*it))
-            PushToken(static_cast<token_type>(*it - '0'));
-        else
-            operate(static_cast<op_type>(*it));
-    }
-    if (stack_.size() > 1)
-        throw std::runtime_error("Operations ended and tack has more than 1 item");
-    result_type ret = stack_.top();
-    stack_ = std::stack<token_type>();
-    return ret;
+	for (std::string::const_iterator it = line.begin(); it != line.end(); ++it) {
+		if (std::isspace(*it))
+			continue;
+		else if (std::isdigit(*it))
+			pushToken(static_cast<token_type>(*it - '0'));
+		else
+			operate(static_cast<op_type>(*it));
+	}
+	if (stack_.size() > 1)
+		throw::std::runtime_error("Operations ended and stack has more than 1 item");
+	result_type ret = stack_.top();
+	stack_ = std::stack<token_type>();
+	return ret;
 }
 
-void    RPN::popOperandsTokens(token_type &lhs, token_type &rhs) {
-    if (stack_.size() < 2)
-        throw std::runtime_error("RPN stack must have at less 2 items to do operations");
-    rhs = stack_.pop();
-    stack_.pop();
-    lhs = stack_.top();
-    stack_.pop();
-}
-
-void    RPN::pushToken(token_type token) {
-    stack_.push(token);
-}
-
-void    RPN::operate(op_type symbol)
+void RPN::popOperandsTokens(token_type &lhs, token_type &rhs)
 {
-    for (short i = 0; i < op._size; ++i) {
-        if (operations_[i].symbol == symbol) {
-            token_type lhs = 0, rhs = 0;
-            popOperandsTokens(lhs, rhs);
-            token_type res = (*(operations_[i].calc)(lhs, rhs));
-            pushToken(res);
-            return ;
-        }
-    }
-    throw std::runtime_error("Encountered and undefined operator");
+	if (stack_.size() < 2)
+		throw std::runtime_error("RPN stack must have at less 2 items to do operations");
+	rhs = stack_.top();
+	stack_.pop();
+	lhs = stack_.top();
+	stack_.pop();
 }
 
-RPN::token_type RPN::opSum(const token_type &lhs, const token_type &rhs) {
-    return lhs + rhs;
+void RPN::pushToken(token_type token)
+{
+	stack_.push(token);
 }
 
-RPN::token_type RPN::opSub(const token_Type &lhs, const token_type &rhs) {
-    return lhs - rhs;
+void RPN::operate(op_type symbol)
+{
+	for (short i = 0; i < op_size; ++i) {
+		if (operations_[i].symbol == symbol) {
+			token_type lhs = 0, rhs = 0;
+			popOperandsTokens(lhs, rhs);
+			token_type res = (*(operations_[i].calc))(lhs, rhs);
+			pushToken(res);
+			return;
+		}
+	}
+	throw std::runtime_error("Encoutered an undefined operator");
 }
 
-RPN::token_type RPN::opSub(const token_type &lhs, const token_type &rhs) {
-    return lhs * rhs;
+RPN::token_type RPN::opSum(const token_type &lhs, const token_type &rhs)
+{
+	return lhs + rhs;
 }
-
-RPN::token_type RPN::opMul(const token_type &lhs, const token_type &rhs) {
-    if (rhs == 0)
-        throw std::runtime_error("Tried to divide by 0");
-    return lhs / rhs;
+RPN::token_type RPN::opSub(const token_type &lhs, const token_type &rhs)
+{
+	return lhs - rhs;
+}
+RPN::token_type RPN::opMul(const token_type &lhs, const token_type &rhs)
+{
+	return lhs * rhs;
+}
+RPN::token_type RPN::opDiv(const token_type &lhs, const token_type &rhs)
+{
+	if (rhs == 0)
+		throw std::runtime_error("Tried to divide by 0");
+	return lhs / rhs;
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -16,7 +16,7 @@
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " \"OPERATION\"n";
+        std::cerr << "Usage: " << argv[0] << " \"OPERATION\"\n";
         return 1;
     }
 
